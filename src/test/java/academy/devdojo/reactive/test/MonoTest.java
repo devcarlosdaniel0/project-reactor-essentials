84c742520967d0103a6bebb01287714018572c53
Carlos Daniel <cadanielmarchesin@gmail.com>
2025-02-10 20:46:02 -0300

    entendendo mono

--- src/test/java/academy/devdojo/reactive/test/MonoTest.java
+++ src/test/java/academy/devdojo/reactive/test/MonoTest.java
@@ -2,12 +2,21 @@ package academy.devdojo.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @Slf4j
 public class MonoTest {
 
     @Test
     void test() {
-        log.info("Hello world!");
+        String name = "Carlos Daniel";
+        Mono<String> mono = Mono.just(name).log();
+
+        mono.subscribe();
+        log.info("---------------");
+        StepVerifier.create(mono)
+                .expectNext(name)
+                .verifyComplete();
     }
 }
