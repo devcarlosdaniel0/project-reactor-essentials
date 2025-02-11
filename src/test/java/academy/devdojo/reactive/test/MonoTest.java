e70f515c9660f972c9b2c375c4f348aaea951f2d
Carlos Daniel <cadanielmarchesin@gmail.com>
2025-02-10 21:23:06 -0300

    Subscribing to Mono with Consumer, Error, Complete, and more

--- src/test/java/academy/devdojo/reactive/test/MonoTest.java
+++ src/test/java/academy/devdojo/reactive/test/MonoTest.java
@@ -2,6 +2,7 @@ package academy.devdojo.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.reactivestreams.Subscription;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -39,9 +40,47 @@ public class MonoTest {
                 .map(s -> {throw new RuntimeException("Testing error mono");});
 
         mono.subscribe(s -> log.info("Name: {}", s), s -> log.error(s.getMessage()));
+        mono.subscribe(s -> log.info("Name: {}", s), Throwable::printStackTrace);
         log.info("---------------");
         StepVerifier.create(mono)
                 .expectError(RuntimeException.class)
                 .verify();
+    }
+
+    @Test
+    void monoSubscriberConsumerComplete() {
+        String name = "Carlos Daniel";
+        Mono<String> mono = Mono.just(name)
+                .log()
+                .map(String::toUpperCase);
+
+        mono.subscribe(s -> log.info("Value: '{}'", s),
+                Throwable::printStackTrace,
+                () -> log.info("Finished"));
+
+        log.info("---------------");
+
+        StepVerifier.create(mono)
+                .expectNext(name.toUpperCase())
+                .verifyComplete();
+    }
+
+    @Test
+    void monoSubscriberConsumerSubscription() {
+        String name = "Carlos Daniel";
+        Mono<String> mono = Mono.just(name)
+                .log()
+                .map(String::toUpperCase);
+
+        mono.subscribe(s -> log.info("Value: '{}'", s),
+                Throwable::printStackTrace,
+                () -> log.info("Finished"),
+                Subscription::cancel);
+
+        log.info("---------------");
+
+        StepVerifier.create(mono)
+                .expectNext(name.toUpperCase())
+                .verifyComplete();
     }
 }
